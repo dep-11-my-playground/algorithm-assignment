340a57ff7bafd15983f975b86ce1fb1e5b3d1a63
Ruwini Thakshila <ruwinithakshila2017@gmail.com>
2023-08-13 20:31:40 +0000

    Modified Algo-5

--- Algo_5.java
+++ Algo_5.java
@@ -6,20 +6,32 @@ public class Algo_5 {
     public static void main(String[] args) {
         
         String input;
+        String output = "";
+        loop:
         do {
             System.out.print("Enter a text: ");
             input = scanner.nextLine();
+            char[] chars = input.toCharArray();
+
+            if(input.isBlank()){
+                System.out.println("The text is empty...Please enter a valid text!");
+                continue loop;
+            }
+
+            for (int i = chars.length-1; i >=0; i--) {
+                int c = chars[i];
+                if (c < 0 || c > 128 ){
+                    System.out.println("The text has included non ASCII characters..Please enter a valid text!");
+                    continue loop;
+                    
+                }else {
+                    output += chars[i];   
+                }       
+            }
+            System.out.println("Reversed text: " + output);
+            break;
             
-        }while(input.isBlank());
-
-        char[] chars = input.toCharArray();
-
-        String output = "";
-        for (int i = chars.length-1; i >=0; i--) {
-            output += chars[i];
-        }
-
-        System.out.println(output);
+        }while(true);
 
     }
 }
